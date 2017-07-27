c1b007bda78281af0775344a70774b27bd81c44f
Maciej Molski-Mazurek <m.molski@samsung.com>
2017-07-27 12:21:44 +0200

    Add enemy mob array with collision, sprites; player movement etc. Next video: 'Tilemaps and Layers'

--- js/play.js
+++ js/play.js
@@ -7,11 +7,21 @@ var playState={
     create: function(){
         var self = this;
         cursors = game.input.keyboard.createCursorKeys(game);
-
-
-
-        // game.input.activePointer.capture = true;
+        self.player = new Player(300,200);
+        game.add.existing(self.player);
         game.physics.enable(self.player, Phaser.Physics.ARCADE);
+
+        self.mob = game.add.group();
+        self.mob.add(Enemy(100,100));
+        self.mob.add(Enemy(200,100));
+        self.mob.add(Enemy(100,200));
+        self.mob.add(Enemy(200,200));
+        self.mob.add(Enemy(400,200));
+        self.mob.forEach(function(enemy,index){
+            game.physics.enable(enemy, Phaser.Physics.ARCADE);
+            enemy.body.immovable = true;
+        });
+        //game.input.activePointer.capture = true;
     },
     update: function(){
         var self = this;
@@ -20,10 +30,10 @@ var playState={
         //     self.player.xDest = game.input.x;
         //     self.player.yDest = game.input.y;
         // }
-        game.physics.arcade.collide(self.player, self.enemy, function(){
-            self.stopplayer();
+        game.physics.arcade.collide(self.player, self.mob, function(){
+            self.player.stop();
         });
-        self.moveplayer();
+        self.player.update();
     }
 }
 
@@ -63,11 +73,46 @@ function Player(x,y){
 
 function Enemy(x,y){
     var enemy = game.add.sprite(x,y,'enemies');
-    
+
+    enemy.xDest = x;
+    enemy.yDest = y;
+
     enemy.frame = 14;
     game.add.existing(enemy);
     game.physics.enable(enemy, Phaser.Physics.ARCADE);
     enemy.anchor.setTo(.5, 1);
     enemy.scale.x = -1;
     enemy.body.immovable = true;
+
+    enemy.gotoXY = function (x,y) {
+        // if(Math.round(self.player.x/10) == Math.round(self.player.xDest/10)){
+        //     self.player.body.velocity.x = 0;
+        // }
+        // else if (Math.floor(self.player.x) < Math.floor(self.player.xDest)){
+        //     self.player.body.velocity.x = 80;
+        //     self.player.scale.x = 1;
+        // }
+        // else if (Math.floor(self.player.x) > Math.floor(self.player.xDest)){
+        //     self.player.body.velocity.x = -80;
+        //     self.player.scale.x = -1;
+        // }
+        // if(Math.round(self.player.y/10) == Math.round(self.player.yDest/10)){
+        //     self.player.body.velocity.y=0;
+        // }
+        // else if (Math.floor(self.player.y) < Math.floor(self.player.yDest)){
+        //     self.player.body.velocity.y = 80;
+        // }
+        // else if (Math.floor(self.player.y) > Math.floor(self.player.yDest)){
+        //     self.player.body.velocity.y = -80;
+        // }
+    }
+
+    enemy.update = function(){
+
+    }
+
+    enemy.stop = function (){
+
+    }
+    return enemy;
 }
